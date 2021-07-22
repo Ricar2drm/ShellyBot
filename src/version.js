e1bb348ab8784510b6b704fb03e3e859676ca5b4
Ricar2drm <87620981+Ricar2drm@users.noreply.github.com>
2021-07-22 21:18:42 +0200

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,26 +1,6 @@
 const version = (prefix, pushname) => {
     return `
-*ѕнαηвσт ву ѕнαη∂υу*
-
-*Actualizado:* 13 de julio de 2021
-*Versión actual:* 3.1
-*Ofrecida por:* ѕнαη∂υу™
-
-
-*INFORME*
-
-Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos
-Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
-
-
-*BLOG DE SHANBOT*
-
-https://cutt.ly/jmSF2RN
-
-ву ѕнαη∂υу
-`
+Aquí no hay nada, no deberías de estar viendo esto 🤝
 
 }
 
